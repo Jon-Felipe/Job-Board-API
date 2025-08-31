faf8dabb4bb1c806095377b39f5f31d7db49c606
Jon-Felipe <rodriguesjf7@gmail.com>
2025-08-31 09:56:37 +0200

    installed and setup dotenv file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express, { type Request, type Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
 
 app.use(express.json());
@@ -7,7 +10,7 @@ app.get('/', (_: Request, res: Response) => {
   res.send('Hello World: TypeScript');
 });
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Job Board server running on port: ${port}`);
 });
