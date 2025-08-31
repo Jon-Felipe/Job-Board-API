85d18ff2fe6d2ffff77a441ef06df3d8c9e27a69
Jon-Felipe <rodriguesjf7@gmail.com>
2025-08-31 12:30:59 +0200

    database connection setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { type Request, type Response } from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db';
+
 dotenv.config();
 
 const app = express();
@@ -10,7 +12,10 @@ app.get('/', (_: Request, res: Response) => {
   res.send('Hello World: TypeScript');
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Job Board server running on port: ${port}`);
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Job Board server running on PORT: ${PORT}`);
+  });
 });
