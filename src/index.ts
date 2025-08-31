2f66f880327746889b8e972626179c7cbdbcd19d
Jon-Felipe <rodriguesjf7@gmail.com>
2025-08-31 09:52:03 +0200

    json parse middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express, { type Request, type Response } from 'express';
 const app = express();
-const port = 3000;
+
+app.use(express.json());
 
 app.get('/', (_: Request, res: Response) => {
   res.send('Hello World: TypeScript');
 });
 
+const port = 3000;
 app.listen(port, () => {
   console.log(`Job Board server running on port: ${port}`);
 });
