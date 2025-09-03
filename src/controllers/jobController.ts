9844118e87c19328414b9f8da79f0515d9d8c266
Jon-Felipe <rodriguesjf7@gmail.com>
2025-09-03 10:25:44 +0200

    get jobs limit query

--- src/controllers/jobController.ts
+++ src/controllers/jobController.ts
@@ -1,9 +1,24 @@
 import { Request, Response } from 'express';
 import Job from '../models/Job';
 
-export async function getJobs(_: Request, res: Response): Promise<void> {
+interface IGetJobsQuery {
+  limit?: string;
+}
+
+export async function getJobs(
+  req: Request<{}, {}, {}, IGetJobsQuery>,
+  res: Response
+): Promise<void> {
+  const limit = parseInt(req.query.limit as string, 10) || 6;
+
+  let query = Job.find({});
+
+  if (limit > 0) {
+    query = query.limit(limit);
+  }
+
   try {
-    const jobs = await Job.find({});
+    const jobs = await query.exec();
     res.status(200).json({ jobs });
   } catch (error) {
     res.status(500).json({ error: (error as Error).message });
